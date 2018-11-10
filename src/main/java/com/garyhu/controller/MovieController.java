593243ed43de2dfcc641fbd85763267c58f5d172
huweiwei <huweiwei@superengine.com.cn>
2018-11-10 18:02:14 +0800

    添加微服务的配置

--- src/main/java/com/garyhu/controller/MovieController.java
+++ src/main/java/com/garyhu/controller/MovieController.java
@@ -1,10 +1,13 @@
 package com.garyhu.controller;
 
 import com.garyhu.entity.User;
-import jdk.nashorn.internal.objects.annotations.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author: garyhu
@@ -18,8 +21,10 @@ public class MovieController {
     @Autowired
     RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/{id}")
-    @ResponseBody
     public User getUser(@PathVariable Integer id){
 
         User user = restTemplate.getForObject("http://localhost:8000/user/" + id, User.class);
@@ -27,4 +32,8 @@ public class MovieController {
         return user;
     }
 
+    @GetMapping("/movie-instance")
+    public List<ServiceInstance> showInfo(){
+        return this.discoveryClient.getInstances("microservice-simple-consumer-movie");
+    }
 }
