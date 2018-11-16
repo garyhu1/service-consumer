8bfdd845bec9dba11b45de0036eef0d23819a20f
huweiwei <huweiwei@superengine.com.cn>
2018-11-16 11:07:21 +0800

    Feign多参数请求

--- src/main/java/com/garyhu/controller/StudentController.java
+++ src/main/java/com/garyhu/controller/StudentController.java
@@ -4,9 +4,10 @@ import com.garyhu.entity.Student;
 import com.garyhu.feign.StudentFeignClient;
 import com.garyhu.pojo.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: garyhu
@@ -22,5 +23,24 @@ public class StudentController {
     @GetMapping("/student")
     public Result<Student> getStduent(@RequestParam(value = "id",required = true)Integer id){
         return studentFeignClient.findById(id);
+    }
+
+    @GetMapping("/getStudent")
+    public Result<Student> findByNameAndAge(@RequestParam("name") String name,@RequestParam("age") Integer age){
+        return studentFeignClient.findByNameAndAge(name,age);
+    }
+
+    @GetMapping("/getStudent2")
+    public Result findByNameAndAge2(@RequestParam("name") String name,@RequestParam("age") Integer age){
+        Map<String,Object> map = new HashMap<>();
+        map.put("name",name);
+        map.put("age",age);
+
+        return studentFeignClient.student(map);
+    }
+
+    @PostMapping("/getStudent3")
+    public Result obtainStudent(@RequestBody Student student){
+        return studentFeignClient.obtainStudent(student);
     }
 }
--- src/main/java/com/garyhu/feign/StudentFeignClient.java
+++ src/main/java/com/garyhu/feign/StudentFeignClient.java
@@ -5,9 +5,12 @@ import com.garyhu.pojo.Result;
 import config.FeignConfiguration;
 import feign.RequestLine;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 /**
  * @author: garyhu
@@ -20,5 +23,18 @@ public interface StudentFeignClient {
     @RequestMapping(value = "/getStudent",method = RequestMethod.GET)
     public Result<Student> findById(@RequestParam(value = "id",required = true) Integer id);
 
-//    @RequestLine("GET /getStudent")
+    /**
+     * 这里不可以使用@RequestBody
+     * @param name
+     * @param age
+     * @return
+     */
+    @RequestMapping(value="/student",method = RequestMethod.GET)
+    public Result<Student> findByNameAndAge(@RequestParam("name")String name,@RequestParam("age")Integer age);
+
+    @RequestMapping(value = "/student",method = RequestMethod.GET)
+    public Result<Student> student(@RequestParam Map<String,Object> map);
+
+    @RequestMapping(value = "/obtainStudent",method = RequestMethod.POST)
+    public Result obtainStudent(@RequestBody Student student);
 }
