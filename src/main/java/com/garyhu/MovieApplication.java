f45fc8ed4bbe0a7a80d3d696a8aa89c8890123cf
garyhu <942984941@qq.com>
2018-11-17 22:13:06 +0800

    添加两个新的依赖：hystrixDashboard+turbine

--- src/main/java/com/garyhu/MovieApplication.java
+++ src/main/java/com/garyhu/MovieApplication.java
@@ -9,6 +9,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,6 +24,10 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients// feign
 // 只要Hystrix在项目的classpath中，Feign就会使用短路器包裹Feign客户端的所有方法
 @EnableHystrix// hystrix容错 或者使用注解@EnableCircuitBreaker
+// 使用hystrix dashboard让监控数据图形化、可视化
+@EnableHystrixDashboard
+// 使用turbine监控多个微服务
+//@EnableTurbine
 public class MovieApplication {
 
     public static void main(String[] args) throws Exception{
